e328ff9c854c47da00790c50d1bf75554e0d986b
Alejorico618 <d.alejandrorico2@gmail.com>
2025-05-31 12:32:01 -0500

    autoshopcontext en carpeta Data

--- AutoShopFin/Data/AutoShopFinContext.cs
+++ AutoShopFin/Data/AutoShopFinContext.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using AutoShopFin.Models;
 
 namespace AutoShopFin.Data
 {
-    class AutoShopFinContext
+    public class AutoShopFinContext : DbContext
     {
+        public AutoShopFinContext(DbContextOptions<AutoShopFinContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Vehiculos> Vehiculos { get; set; }
+        public DbSet<Reparaciones> Reparaciones { get; set; }
+        public DbSet<Partes> Partes { get; set; }
+        public DbSet<Citas> Citas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Citas>()
+                .HasOne(c => c.Vehiculo)
+                .WithMany()
+                .HasForeignKey(c => c.IdVehiculo)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Citas>()
+                .HasOne(c => c.Cliente)
+                .WithMany()
+                .HasForeignKey(c => c.IdCliente)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
